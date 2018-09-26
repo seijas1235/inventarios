d7913cff72c6ac0738c974cf4159e07ecdb92334
unknown <cmorales@vrinfosysgt.com>
2018-09-26 11:30:29 -0600

    CRUD unidaddes de medida
    Signed-off-by: unknown <cmorales@vrinfosysgt.com>

--- app/Http/Controllers/UnidadesDeMedidaController.php
+++ app/Http/Controllers/UnidadesDeMedidaController.php
@@ -3,10 +3,188 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
+use DB;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+use App\UnidadDeMedida;
+Use App\User;
 
 class UnidadesDeMedidaController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
+    public function index()
+    {
+
+        return view ("unidades_de_medida.index");
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view("unidades_de_medida.create");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {       
+        $data = $request->all();
+        $unidad_de_medida = UnidadDeMedida::create($data);
+
+        return Response::json($unidad_de_medida);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UnidadDeMedida $unidad_de_medida)
+    {
+        $query = "SELECT * FROM unidades_de_medida WHERE id=".$unidad_de_medida->id."";
+        $fieldsArray = DB::select($query);
+
+        return view('unidades_de_medida.edit', compact('unidad_de_medida', 'fieldsArray'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UnidadDeMedida $unidad_de_medida, Request $request)
+    {
+        Response::json( $this->updateUnidadDeMedida($unidad_de_medida , $request->all()));
+        return redirect('/unidades_de_medida');
+    }
+
+    public function updateUnidadDeMedida(UnidadDeMedida $unidad_de_medida, array $data )
+    {
+        $id= $unidad_de_medida->id;
+        $unidad_de_medida->descripcion = $data["descripcion"];
+        $unidad_de_medida->cantidad = $data["cantidad"];
+        $unidad_de_medida->save();
+
+        return $unidad_de_medida;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UnidadDeMedida $unidad_de_medida, Request $request)
+    {
+        $user1= Auth::user()->password;
+
+        if ($request["password_delete"] == "")
+        {
+            $response["password_delete"]  = "La contraseña es requerida";
+            return Response::json( $response  , 422 );
+        }
+        else if( password_verify( $request["password_delete"] , $user1))
+        {
+            $id= $unidad_de_medida->id;
+            $unidad_de_medida->delete();
+            
+            $response["response"] = "El tipo vehiculo ha sido eliminado";
+            return Response::json( $response );
+        }
+        else {
+            $response["password_delete"] = "La contraseña no coincide";
+            return Response::json( $response  , 422 );
+        }    
+    }
+
+
+    public function getJson(Request $params)
+    {
+        $api_Result = array();
+        // Create a mapping of our query fields in the order that will be shown in datatable.
+        $columnsMapping = array("id", "descripcion");
+
+        // Initialize query (get all)
+
+        $api_logsQueriable = DB::table('unidades_de_medida');
+        $api_Result['recordsTotal'] = $api_logsQueriable->count();
+
+        $query = "SELECT * FROM unidades_de_medida";
+
+        $where = "";
+
+        if (isset($params->search['value']) && !empty($params->search['value'])){
+
+            foreach ($columnsMapping as $column) {
+                if (strlen($where) == 0) {
+                    $where .=" and (".$column." like  '%".$params->search['value']."%' ";
+                } else {
+                    $where .=" or ".$column." like  '%".$params->search['value']."%' ";
+                }
+
+            }
+            $where .= ') ';
+        }
+        $condition = " ";
+        $query = $query . $condition . $where;
+
+        // Sorting
+        $sort = "";
+        foreach ($params->order as $order) {
+            if (strlen($sort) == 0) {
+                $sort .= 'order by ' . $columnsMapping[$order['column']] . ' '. $order['dir']. ' ';
+            } else {
+                $sort .= ', '. $columnsMapping[$order['column']] . ' '. $order['dir']. ' ';
+            }
+        }
+
+        $result = DB::select($query);
+        $api_Result['recordsFiltered'] = count($result);
+
+        $filter = " limit ".$params->length." offset ".$params->start."";
+
+        $query .= $sort . $filter;
+
+        $result = DB::select($query);
+        $api_Result['data'] = $result;
+
+        return Response::json( $api_Result );
+    }
 }
