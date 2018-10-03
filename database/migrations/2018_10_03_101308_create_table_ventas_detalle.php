0a33187e03009b3b2fdaab49fc93de67f8498fd1
gseijas13 <gseijas@vrinfosysgt.com>
2018-10-03 10:14:59 -0600

    Crud Detalle Ventas

---
+++ database/migrations/2018_10_03_101308_create_table_ventas_detalle.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableVentasDetalle extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ventas_detalle', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('ventas_detalle');
+    }
+}
