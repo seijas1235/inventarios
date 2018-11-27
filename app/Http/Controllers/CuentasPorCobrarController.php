87b9616343b83e221bc15c7564f17da372db2f5a
gseijas13 <gseijas@vrinfosysgt.com>
2018-11-27 11:47:33 -0600

    Seccion Cuentas por cobrar modificanco INDEX

--- app/Http/Controllers/CuentasPorCobrarController.php
+++ app/Http/Controllers/CuentasPorCobrarController.php
@@ -160,51 +160,8 @@ class CuentasPorCobrarController extends Controller
 
     public function getJson(Request $params)
     {
-        $api_Result = array();
-        // Create a mapping of our query fields in the order that will be shown in datatable.
-        $columnsMapping = array("cpc.id", "cpc.cliente_id","cpc.total", "c.nombres", "c.apellidos");
-
-        // Initialize query (get all)
-
-        $api_logsQueriable = DB::table('precios_producto');
-        $api_Result['recordsTotal'] = $api_logsQueriable->count();
-
         $query = "SELECT cpc.id, cpc.cliente_id, cpc.total, concat( c.nombres,' ', c.apellidos) as nombre FROM cuentas_por_cobrar cpc 
         INNER JOIN clientes c on c.id = cpc.cliente_id";
-
-        $where = "";
-
-        if (isset($params->search['value']) && !empty($params->search['value'])){
-
-            foreach ($columnsMapping as $column) {
-                if (strlen($where) == 0) {
-                    $where .=" and (".$column." like  '%".$params->search['value']."%' ";
-                } else {
-                    $where .=" or ".$column." like  '%".$params->search['value']."%' ";
-                }
-
-            }
-            $where .= ') ';
-        }
-        $condition = " ";
-        $query = $query . $condition . $where;
-
-        // Sorting
-        $sort = "";
-        foreach ($params->order as $order) {
-            if (strlen($sort) == 0) {
-                $sort .= 'order by ' . $columnsMapping[$order['column']] . ' '. $order['dir']. ' ';
-            } else {
-                $sort .= ', '. $columnsMapping[$order['column']] . ' '. $order['dir']. ' ';
-            }
-        }
-
-        $result = DB::select($query);
-        $api_Result['recordsFiltered'] = count($result);
-
-        $filter = " limit ".$params->length." offset ".$params->start."";
-
-        $query .= $sort . $filter;
 
         $result = DB::select($query);
         $api_Result['data'] = $result;
