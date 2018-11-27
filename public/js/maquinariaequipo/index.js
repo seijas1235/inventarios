51152958f32f170a8d372477044740fb7cdc83ef
gseijas13 <gseijas@vrinfosysgt.com>
2018-11-27 10:23:58 -0600

    refactorizar maquinaria y equipo

--- public/js/maquinariaequipo/index.js
+++ public/js/maquinariaequipo/index.js
@@ -1,16 +1,11 @@
 var maquinariaequipo_table = $('#maquinariaequipo-table').DataTable({
     "ajax": "/maquinarias_equipo/getJson",
-    "responsive": true,
-    "processing": true,
-    "serverSide": true,
-    "info": true,
-    "showNEntries": true,
     "dom": 'Bfrtip',
     "buttons": [
     {
         extend: 'pdfHtml5',
         exportOptions: {
-            columns: [ 0, 1, 2, 3, 4, 5,6]
+            columns: [ 0, 1, 2, 3, 4, 5]
         }
     },
     'excelHtml5',
